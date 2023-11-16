aa4098966a96cd70bb734677063a125a9e7f35ca
Krr0ptioN <mardin-pop@protonmail.com>
2023-11-16 23:00:08 +0300

    fix(prisma): custom prisma client

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaModule } from 'nestjs-prisma';
+import { CustomPrismaModule } from 'nestjs-prisma';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MusicModule } from './music/music.module';
+import { PrismaClient } from '@prisma/client/data-access';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule.forRoot()],
+  imports: [
+    CustomPrismaModule.forRoot({
+      name: 'PrismaServiceAuth',
+      client: new PrismaClient(),
+    }),
+    MusicModule,
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
