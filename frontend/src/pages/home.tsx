9256bde18d250e4cda11993970ea3ee3b8c97b89
Krr0ptioN <mardin-pop@protonmail.com>
2023-11-16 17:28:17 +0300

    feat(ui): music player

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 export const HomePage = (props: {}) => {
   return (
     <div className="flex justify-center items-center w-screen h-screen">
-      <div className="w-1/3"></div>
+      <MusicPlayer
+      ></MusicPlayer>
     </div>
   );
 };
