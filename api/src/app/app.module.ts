4ffbb2aaf95866f2dd74eb57a459b03dca307a86
Krr0ptioN <mardin-pop@protonmail.com>
2023-11-16 22:05:16 +0300

    feat: setup prisma and database

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { PrismaModule } from 'nestjs-prisma';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,7 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
@@ -15,7 +11,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `(  ) Application is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 
