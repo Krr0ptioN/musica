c7ea067384b07a5c82032d2a3bfaabeef6ca1ee8
Krr0ptioN <mardin-pop@protonmail.com>
2023-11-24 02:43:04 +0300

    fix(test): server config

--- api-e2e/src/support/test-setup.ts
+++ api-e2e/src/support/test-setup.ts
@@ -2,9 +2,9 @@
 
 import axios from 'axios';
 
-module.exports = async function () {
+module.exports = async function() {
   // Configure axios for tests to use.
-  const host = process.env.HOST ?? 'localhost';
-  const port = process.env.PORT ?? '3000';
+  const host = process.env.HOST_API ?? 'localhost';
+  const port = process.env.PORT_API ?? '3000';
   axios.defaults.baseURL = `http://${host}:${port}`;
 };
