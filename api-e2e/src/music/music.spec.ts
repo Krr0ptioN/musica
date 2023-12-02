9fedeb5eaa5b376bf8c0c1ebd47ff8e557b26591
Krr0ptioN <mardin-pop@protonmail.com>
2023-12-02 19:03:18 +0300

    refactor(module/music): e2e

--- api-e2e/src/music/music.spec.ts
+++ api-e2e/src/music/music.spec.ts
@@ -1,48 +1,52 @@
 import axios from 'axios';
-import FormData from 'form-data';
-import { fileStreamByPath } from '../utils';
+import { AppModule } from '@musica/api';
 import { MockData } from '../mock-data';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
 
-describe('POST /api/v1/music | Music creation', () => {
-  it('should throw error if file is not provided', async () => {
-    try {
-      await axios.post(`/api/v1/music`, {
-        name: 'A music without file',
-      });
-    } catch (error) {
-      expect(error.response.status).toBe(400);
-      expect(error.response.data.message).toEqual('No file uploaded');
-    }
+import * as request from 'supertest';
+
+describe('Music Module', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
   });
 
-  it('should throw error if file is not an audio', async () => {
-    const formData = new FormData();
-    const fileStream = fileStreamByPath(
-      MockData.musicCreation.invalidMusicFile.filepath
-    );
-    formData.append('file', fileStream);
-    formData.append('name', MockData.musicCreation.validMusicFile.name);
-    try {
-      await axios.post(`api/v1/music`, formData);
-    } catch (err) {
-      expect(err.response.status).toBe(400);
-      expect(err.response.data.message).toEqual(
-        'Only audio files (mp3, ogg, m4a) are allowed!'
-      );
-    }
+  describe('POST /api/v1/music | Music creation', () => {
+    it('should throw error if file is not provided', async () => { });
+    it('should throw error if file is not an audio', async () => { });
+    it('should create music and upload file', async () => { });
   });
 
-  it('should create music and upload file', async () => {
-    const formData = new FormData();
-    const fileStream = fileStreamByPath(
-      MockData.musicCreation.validMusicFile.filepath
-    );
-    formData.append('file', fileStream);
-    formData.append('name', MockData.musicCreation.validMusicFile.name);
-    const res = await axios.post(`api/v1/music`, formData);
-    expect(res.data.success).toBe(true);
-    expect(res.data.data.date).toBeDefined();
-    expect(res.data.data.fileName).toBeDefined();
-    expect(res.data.data.id).toBeDefined();
+  describe('GET /api/v1/music/file/:id | Get music file', () => {
+    it('should find the music and get the file to play', async () => { });
+  });
+
+  describe('GET /api/v1/music | Get music file', () => {
+    it('should return all music records with default parameters', async () => { });
+    it('should return music records based on specific query parameters', async () => { });
+    it('should handle invalid JSON in query parameters gracefully', async () => { });
+  });
+
+  describe('GET /api/v1/music/:id | Get music file', () => {
+    it('should retrieve a music by ID (findOne)', async () => { });
+  });
+
+  describe('PATCH /api/v1/music/:id | Get music file', () => {
+    it('should update a music by ID (update)', async () => { });
+  });
+
+  describe('DELETE /api/v1/music/:id | Get music file', () => {
+    it('should remove a music by ID (remove)', async () => { });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
