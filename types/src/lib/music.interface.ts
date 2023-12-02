cf00c2a55c02fbdda7fb67267a2630ffedc5ddb9
Krr0ptioN <mardin-pop@protonmail.com>
2023-12-02 13:06:19 +0300

    feat: music type for store and api

---
+++ types/src/lib/music.interface.ts
@@ -0,0 +1,9 @@
+export interface Music {
+  artists: string[] | null;
+  album: string | null;
+  name: string;
+  file: File | null;
+  fileName: string;
+  playlists: string[] | null;
+  id: string;
+}
