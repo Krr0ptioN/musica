a75d06d0be4fe6724627246adac129f85cf363df
Krr0ptioN <mardin-pop@protonmail.com>
2023-12-09 10:47:33 +0300

    chore: remove logs

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { MusicPlayer } from '../widget/MusicPlayer';
 import { useActions } from '../hooks/action';
-import { useSelector } from 'react-redux';
 
 export const HomePage = () => {
   const { getAllMusics } = useActions();
   getAllMusics();
-  const state = useSelector((state) => state);
-  console.log(state);
   return (
     <div className="flex justify-center items-center w-screen h-screen">
       <MusicPlayer />
