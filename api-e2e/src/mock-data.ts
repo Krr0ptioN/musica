1b541e5b13af66c8c17824b14e600642ad19e4c6
Krr0ptioN <mardin-pop@protonmail.com>
2023-12-17 12:13:37 +0300

    revert: e2e tests for backend and frontend

---
+++ api-e2e/src/mock-data.ts
@@ -0,0 +1,14 @@
+export let MockData = {
+  musicCreation: {
+    invalidMusicFile: {
+      filepath: 'api-e2e/assets/non-audio-file.png',
+      name: 'A music with invalid file format',
+      id: 'unset',
+    },
+    validMusicFile: {
+      filepath: 'api-e2e/assets/audio-file.mp3',
+      name: 'A music with valid file format',
+      id: 'unset',
+    },
+  },
+};
