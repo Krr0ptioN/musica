023c2d6ef12ff7c9cabdbd3d353c2a86697fad7a
Krr0ptioN <mardin-pop@protonmail.com>
2023-12-31 22:12:22 +0300

    purge: api-e2e

--- api-e2e/src/utils.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-import fs from 'fs';
-
-export const fileStreamByPath = (filePath: string) => {
-  const cwd = process.cwd();
-  const absolutePathToFile = path.resolve(cwd, filePath);
-  return fs.createReadStream(absolutePathToFile);
-};
