d4a74b47556d66e9f736f0acd6ceead842172538
Krr0ptioN <mardin-pop@protonmail.com>
2024-01-10 12:50:00 +0300

    fix: misuses of valid() from Joi

--- libs/core/src/lib/config/config.ts
+++ libs/core/src/lib/config/config.ts
@@ -2,21 +2,22 @@ import { ENV_NAME } from './env/names.enum';
 
 export const configuration = () => {
   return {
-    portApi: process.env['PORT_API'],
-    helmetEnabled: process.env['HELMET_ENABLED'],
-    corsEnabled: process.env['CORS_ENABLED'],
-    swaggerEnabled: process.env['CORS_ENABLED'],
+    portApi: process.env[ENV_NAME.PORT_API],
+    helmetEnabled: process.env[ENV_NAME.HELMET_ENABLED],
+    corsEnabled: process.env[ENV_NAME.CORS_ENABLED],
+    swaggerEnabled: process.env[ENV_NAME.CORS_ENABLED],
     swaggerTitle: process.env[ENV_NAME.SWAGGER_TITLE],
-    swaggerDescription: process.env['SWAGGER_DESC'],
-    swaggerPath: process.env['SWAGGER_PATH'],
-    swaggerUser: process.env['SWAGGER_USER'],
-    swaggerPassword: process.env['SWAGGER_PASSWORD'],
-    jwtExpiresIn: process.env['JWT_EXPIRES_IN'],
-    jwtRefreshIn: process.env['JWT_REFRESH_IN'],
-    bcryptSaltRound: process.env['BCRYPT_SALT_ROUND'],
-    passwordResetTokenExpiresIn: process.env['PASSWORD_RESET_TOKEN_EXPIRES_IN'],
-    storageDest: process.env['STORAGE_DEST'],
-    musicAudioStorageDest: process.env['STORAGE_DEST'] + '/musics',
-    coverImageStorageDest: process.env['STORAGE_DEST'] + '/covers',
+    swaggerDescription: process.env[ENV_NAME.SWAGGER_DESC],
+    swaggerPath: process.env[ENV_NAME.SWAGGER_PATH],
+    swaggerUser: process.env[ENV_NAME.SWAGGER_USER],
+    swaggerPassword: process.env[ENV_NAME.SWAGGER_PASSWORD],
+    jwtExpiresIn: process.env[ENV_NAME.JWT_EXPIRES_IN],
+    jwtRefreshIn: process.env[ENV_NAME.JWT_REFRESH_IN],
+    bcryptSaltRound: process.env[ENV_NAME.BCRYPT_SALT_ROUND],
+    passwordResetTokenExpiresIn:
+      process.env[ENV_NAME.PASSWORD_RESET_TOKEN_EXPIRES_IN],
+    storageDest: process.env[ENV_NAME.STORAGE_DEST],
+    musicAudioStorageDest: process.env[ENV_NAME.STORAGE_DEST] + '/musics',
+    coverImageStorageDest: process.env[ENV_NAME.STORAGE_DEST] + '/covers',
   };
 };
--- libs/core/src/lib/config/validation.ts
+++ libs/core/src/lib/config/validation.ts
@@ -2,66 +2,25 @@ import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
   // System
-  MONGO_URI: Joi.string()
-    .valid(['development', 'production', 'test'])
-    .required(),
-  PORT_API: Joi.number()
-    .valid(['development', 'production', 'test'])
-    .required(),
-  HOST_API: Joi.string()
-    .valid(['development', 'production', 'test'])
-    .required(),
-  STORAGE_DEST: Joi.string()
-    .valid(['development', 'production', 'test'])
-    .required(),
+  MONGO_URI: Joi.string().required(),
+  PORT_API: Joi.number().required(),
+  STORAGE_DEST: Joi.string().required(),
 
   // Swaggger configuration
-  SWAGGER_ENABLED: Joi.boolean()
-    .valid(['development', 'production'])
-    .optional()
-    .default(true),
-  SWAGGER_TITLE: Joi.string()
-    .valid(['development', 'production'])
-    .optional()
-    .default('Musica API'),
+  SWAGGER_ENABLED: Joi.boolean().optional().default(true),
+  SWAGGER_TITLE: Joi.string().optional().default('Musica API'),
   SWAGGER_DESC: Joi.string()
-    .valid(['development', 'production'])
     .optional()
     .default('Swagger API Documentation server'),
-  SWAGGER_PATH: Joi.string()
-    .valid(['development', 'production'])
-    .optional()
-    .default('swg'),
-  SWAGGER_USER: Joi.string()
-    .valid(['development', 'production'])
-    .optional()
-    .default('admin'),
-  SWAGGER_PASSWORD: Joi.string()
-    .valid(['development', 'production'])
-    .optional()
-    .default('4#2M0!s0D1N#2398@M1N233l'),
+  SWAGGER_PATH: Joi.string().optional().default('swg'),
+  SWAGGER_USER: Joi.string().optional().default('admin'),
+  SWAGGER_PASSWORD: Joi.string().optional().default('4#2M0!s0D1N#2398@M1N233l'),
 
   // Security
-  CORS_ENABLED: Joi.boolean()
-    .valid(['development', 'production', 'test'])
-    .optional(),
-  HELMET_ENABLED: Joi.boolean()
-    .valid(['development', 'production', 'test'])
-    .optional(),
-  JWT_EXPIRES_IN: Joi.string()
-    .valid(['development', 'production', 'test'])
-    .optional()
-    .default('5m'),
-  JWT_REFRESH_IN: Joi.string()
-    .valid(['development', 'production', 'test'])
-    .optional()
-    .default('7d'),
-  BCRYPT_SALT_ROUND: Joi.number()
-    .valid(['development', 'production', 'test'])
-    .optional()
-    .default(10),
-  PASSWORD_RESET_TOKEN_EXPIRES_IN: Joi.string()
-    .valid(['development', 'production', 'test'])
-    .optional()
-    .default('5m'),
+  CORS_ENABLED: Joi.boolean().optional(),
+  HELMET_ENABLED: Joi.boolean().optional(),
+  JWT_EXPIRES_IN: Joi.string().optional().default('5m'),
+  JWT_REFRESH_IN: Joi.string().optional().default('7d'),
+  BCRYPT_SALT_ROUND: Joi.number().optional().default(10),
+  PASSWORD_RESET_TOKEN_EXPIRES_IN: Joi.string().optional().default('5m'),
 });
