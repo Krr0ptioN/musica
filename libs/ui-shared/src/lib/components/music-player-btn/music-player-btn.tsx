91ed754a2a3842a4319038d7aa8878c5cc534653
Krr0ptioN <mardin-pop@protonmail.com>
2024-01-12 13:38:42 +0300

    fix(ui-shared-lib): keyboard listener to provide MusicPlayerBtn accessibility while user have no access to mouse

--- libs/ui-shared/src/lib/components/music-player-btn/music-player-btn.tsx
+++ libs/ui-shared/src/lib/components/music-player-btn/music-player-btn.tsx
@@ -6,11 +6,20 @@ export interface MusicPlayerBtnProps {
 }
 
 export function MusicPlayerBtn(props: MusicPlayerBtnProps) {
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLElement>) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      props.handlerOnClick();
+    }
+  };
   return (
     <i
+      tabIndex={0}
       className={`${props.className} p-3 hover:text-gray-700`}
       title={props.title}
       onClick={props.handlerOnClick}
+      onKeyPress={handleKeyPress}
+      role="button"
+      onKeyDown={handleKeyPress}
     ></i>
   );
 }
